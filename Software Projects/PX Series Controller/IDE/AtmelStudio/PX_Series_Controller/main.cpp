11e16645109414b169f39b7560df4aea29cac237
Andrew Taylor-Steen <31416824+atsteen@users.noreply.github.com>
2018-09-02 09:24:42 -0700

    Solution file added for AVR compliation of existing codebase
    Added new solution created in Atmel Studio v7.  This new solution will link existing source as well as manage concrete component implementations for AVR support.

---
+++ Software Projects/PX Series Controller/IDE/AtmelStudio/PX_Series_Controller/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * PX_Series_Controller.cpp
+ *
+ * Created: 9/2/2018 9:07:48 AM
+ * Author : publi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
