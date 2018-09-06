4508a4592bbcd3b08ae0badbec6dc8ac379344d3
Andrew Taylor-Steen <31416824+atsteen@users.noreply.github.com>
2018-09-06 11:26:42 -0700

    Adding updated Atmel project
    The Atmel project added in this commit was imported from a template arduino sketch.  Creating an Arduino project in atmel studio automatically includes and configures toolchain and device options necessary to program and hardware debug ATMega Microprocessors.

---
+++ Software Projects/PX Series Controller/IDE/AtmelStudio/PX_Series_Controller/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 9/6/2018 11:22:06 AM
+ * Author : publi
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ Software Projects/PX Series Controller/IDE/AtmelStudio/PX_Series_Controller/PX4_Launcher/Sketch.cpp
@@ -0,0 +1,13 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+/*End of auto generated code by Atmel studio */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
